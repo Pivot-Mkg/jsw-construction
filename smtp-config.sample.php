cc90959949008cd674f1ed6389d93e1a4fb4a154
AAKASH <aakash@example.com>
2026-01-19 16:11:19 +0530

    mailer

---
+++ smtp-config.sample.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    // SMTP server settings
+    'host' => '',
+    'port' => '',
+    'username' => '',
+    'password' => '',
+    'secure' => '', // starttls, tls, ssl, none
+
+    // Sender/recipient
+    'from_email' => '',
+    'from_name' => '',
+    'to_email' => '',
+];
